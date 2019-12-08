37de24ba85c2df696430c7527f3753b17d86cff6
Emmanuel Guerra <emmanuelnypd@gmail.com>
2019-12-08 01:11:21 -0400

    Created MovieDetailActivity and the movie header layout

---
+++ app/src/main/java/com/eagskunst/libraries/movieapp/ui/movie_detail/MovieDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.eagskunst.libraries.movieapp.ui.movie_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eagskunst.libraries.movieapp.R
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
